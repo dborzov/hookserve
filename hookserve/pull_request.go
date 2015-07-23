b8e519d7db08777391b33d5b29e1e0a44d375dcc
Dmitry Borzov <tihoutrom@gmail.com>
2015-07-23 11:19:00 -0400

    No need to escape if JSON is not formatted properly

--- hookserve/pull_request.go
+++ hookserve/pull_request.go
@@ -5,32 +5,11 @@ import "github.com/bmatsuo/go-jsontree"
 func parsePullRequst(event *Event, request *jsontree.JsonTree) (err error) {
 	// Fill in values
 	event.Owner, err = request.Get("pull_request").Get("head").Get("repo").Get("owner").Get("login").String()
-	if err != nil {
-		return err
-	}
 	event.Repo, err = request.Get("pull_request").Get("head").Get("repo").Get("name").String()
-	if err != nil {
-		return err
-	}
 	event.Branch, err = request.Get("pull_request").Get("head").Get("ref").String()
-	if err != nil {
-		return err
-	}
 	event.Commit, err = request.Get("pull_request").Get("head").Get("sha").String()
-	if err != nil {
-		return err
-	}
 	event.BaseOwner, err = request.Get("pull_request").Get("base").Get("repo").Get("owner").Get("login").String()
-	if err != nil {
-		return err
-	}
 	event.BaseRepo, err = request.Get("pull_request").Get("base").Get("repo").Get("name").String()
-	if err != nil {
-		return err
-	}
 	event.BaseBranch, err = request.Get("pull_request").Get("base").Get("ref").String()
-	if err != nil {
-		return err
-	}
 	return nil
 }
